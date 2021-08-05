4bbf26f98d9349e17e95c248c53176fe0b670d31
Isis Pedroni <isispedroni3@gmail.com>
2021-08-04 23:30:43 -0300

    alteraçoes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,39 @@
 import React from 'react'
 import "./Header.css"
+// import SearchIcon from '@material-ui/icons/Search';
+
 
 
 
 
 
 function Header() {
-
+    function clicou ( ) {  
+        alert ( 'Pagina em construção!' ) ;
+      }
     return (
         <div className="header">
-
-            {/* <img
+            <div className="Engloba">
+            <img
                 className="header_logo"
-                src="https://pngimg.com/uploads/amazon/amazon_PNG25.png" alt="Logo" /> */}
+                src="https://image.flaticon.com/icons/png/512/4342/4342171.png" alt="Logo" />
             <div className="nome_marca">
                 <h4>EscolherNome</h4>
             </div>
+            </div>
+        
+            <div className="engloba_header">
+            
+            <button onClick = {clicou}> login </button> 
+            
             <img
                 className="carrinho_menu"
-                src="https://image.flaticon.com/icons/png/512/46/46297.png" alt="Logo" />
+                src="https://image.flaticon.com/icons/png/512/3737/3737372.png" alt="Logo" />
+            
+            </div>
 
-
+     
+            
         </div>
     )
 }
