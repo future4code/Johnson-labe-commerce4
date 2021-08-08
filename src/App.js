825472a3a710179e8358cc193a841adcb55aedf1
AndreLuizGomesPereira <andregomesenator@gmail.com>
2021-08-08 00:57:20 -0300

    Iniciado base do projeto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
 import React from "react";
 import Header from "./components/Header";
-import Venda from './components/Venda'
+import Filtro from "./components/Filtro";
+import Produto from "./components/Produto";
+import Carrinho from "./components/Carrinho";
 import Footer from "./components/Footer";
-import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Venda />
-      <Footer />
-    </div>
-  );
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  gap: 4px;
+`;
+
+class App extends React.Component {
+  state = {
+    carrinho: [],
+  };
+
+  //Ação para adicionar no carrinho
+  onClickAdicionarCarrinho = (produto) => {
+    // Criar uma cópia dos itens do carrinho
+    const novoCarrinho = [...this.state.carrinho];
+    novoCarrinho.push(produto);
+    this.setState({ carrinho: novoCarrinho });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <AppContainer>
+          <Filtro />
+          <Produto onClickAdicionarCarrinho={this.onClickAdicionarCarrinho}/>
+          <Carrinho produtosCarrinho={this.state.carrinho} />
+        </AppContainer>
+
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
